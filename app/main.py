623ff58ad5155480e2727619a6d0cae1bb08e233
Jenkins CI <jenkins@ci.local>
2026-04-03 17:52:38 +0530

    Intentionally break health check for rollback demo

--- app/main.py
+++ app/main.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Python CI/CD Kubernetes App is running!", 200
+    return "Hello"
 
 @app.route("/health")
 def health():
-    return "OK", 200
+    return 1 / 0
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
